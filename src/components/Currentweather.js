b10c72e359ca58572ddd154e349a8cf11a6044d0
Tinky gorai <109448557+21410032@users.noreply.github.com>
2023-03-30 10:22:18 +0530

    Add files via upload

---
+++ src/components/Currentweather.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './currentweather.css'
+function Currentweather({data}) {
+  return (
+    <div className='currentweather'>
+        <div className='top'>
+            <div>         
+         <p className='city'>{data.city}</p>
+         <p className='weather-desc'>{data.weather[0].description}</p>
+         </div>
+          <div>
+            <img alt="weather" src={`icon/${data.weather[0].icon}.png`}/>
+          </div>
+
+        </div>
+        <div className='bottom'>
+         <p className='tempreture'>{Math.round(data.main.temp)-273}°C</p>
+         <div className='details'>
+            <div className='parameter-row'>
+                <div className='parameter-lavel'>
+                 Feels Like:
+                </div>
+                <div className='parameter-value'>
+                {Math.round(data.main.feels_like)-273}°C
+                </div>
+            </div>
+            <div className='parameter-row'>
+                <div className='parameter-lavel'>
+                 Humidity:
+                </div>
+                <div className='parameter-value'>
+                {data.main.humidity}%
+                </div>
+                {/* <{data.weather.icon}/> */}
+            </div>
+            <div className='parameter-row'>
+                <div className='parameter-lavel'>
+                 Wind:
+                </div>
+                <div className='parameter-value'>
+                {data.wind.speed} & {data.wind.degree}
+                </div>
+            </div>
+            <div className='parameter-row'>
+                <div className='parameter-lavel'>
+                 pressure:
+                </div>
+                <div className='parameter-value'>
+                {data.main.pressure}
+                </div>
+            </div>
+         </div>
+        </div>
+    </div>
+  )
+}
+
+export default Currentweather
